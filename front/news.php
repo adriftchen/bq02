6eb9b40476bfa2c10533c10e3c6c1a03754822a4
linkzepp <adriftchen@gmail.com>
2020-12-21 13:40:36 +0800

    完成最新文章顯示及分頁和切換詳細內容的功能

--- front/news.php
+++ front/news.php
@@ -7,11 +7,17 @@
       <td width="20%"></td>
     </tr>
     <?php
-      $all=$News->all(['sh'=>1]);
+      $count=$News->count(['sh'=>1]);
+      $div=5;
+      $pages=ceil($count/$div);
+      $now=(isset($_GET['p']))?$_GET['p']:1;
+      $start=($now-1)*$div;
+    
+      $all=$News->all(['sh'=>1]," limit $start,$div");
       foreach($all as $news){
     ?>
     <tr>
-      <td id="t<?=$news['id'];?>"><?=$news['title'];?></td>
+      <td class="header" id="t<?=$news['id'];?>" style="cursor:pointer;color:blue;text-decoration:underline"><?=$news['title'];?></td> <!-- 設定class="header" 底下寫js -->
       <td>
           <span class="title"><?=mb_substr($news['text'],0,30,'utf8');?>...</span>
           <span class="text" style="display:none"><?=nl2br($news['text']);?></span>  <!-- nl2br(string) 函數在字符串中的每個新行(\n) 之前插入HTML 換行符(<br />)。 -->
@@ -22,4 +28,29 @@
     }
     ?>
   </table>
+  <div class="ct">
+
+<?php
+  if(($now-1)>0){
+    echo "<a href='index.php?do=news&p=" .($now-1)."' > &lt;  </a>";
+  }
+
+  for($i=1;$i<=$pages;$i++){
+    $fontsize=($i==$now)?"28px":"18px";
+    echo "<a href='index.php?do=news&p=$i' style='font-size:$fontsize'> $i </a>"; //製作頁碼
+  }
+
+  if(($now+1)<=$pages){
+    echo "<a href='index.php?do=news&p=" .($now+1)."' > &gt;  </a>";
+  }
+?>
+
+</div>
 </fieldset>
+
+<script>
+$(".header").on("click",function(){
+  $(this).next().children('.title').toggle() /* 一個隱藏 另一個就顯示 */
+  $(this).next().children('.text').toggle()
+})
+</script>
